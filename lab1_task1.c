4da31568908b36c106178c2528a9f21bb9e84610
5kyHam5ter <92105017+5kyHam5ter@users.noreply.github.com>
2026-09-08 12:48:18 +0300

    Update lab1_task1.c

--- lab1_task1.c
+++ lab1_task1.c
@@ -1,60 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    float a;
-    float b;
-    float c;
-    int input_check_a;
-    int input_check_b;
-    int input_check_c;
-    float res_ab;
-    float res_bc;
-    float res_ac;
-    int cnt = 0;
+    float radius;
+    float square;
+    float volume;
+    int input_check;
     
-    printf("Enter number A... ");
-    input_check_a = scanf("%f", &a);
-    printf("%d", input_check_a);
-
-    if (input_check_a !=1) {
-    printf("Invalid input");
-    return 0;
-    } 
-        else
-        printf("Enter number B... ");
-        input_check_b = scanf("%f", &b);
-
-            if (input_check_b !=1) {
-                printf("Invalid input");
-                return 0;
-        }
-            else
-            printf("Enter number C... ");
-            input_check_c = scanf("%f", &c);
-                if (input_check_c !=1) {
-                    printf("\nInvalid input");
-                    return 0;
-            }
-                else
-                    printf("");   
-    res_ab = a*b;
-    if (res_ab==1)
-    cnt++;
+    printf("Enter a radius... ");
+    input_check = scanf("%f", &radius);
+    square = 4*radius*radius*3.14;
+    volume = 3.14*radius*radius*radius*4/3;
+    if (input_check != 1)
+    printf("Invalid input.");
     else
-    printf("");
-    
-    res_bc = b*c;
-    if (res_bc==1)
-    cnt++;
-    else
-    printf("");
-    
-    res_ac = a*c;
-    if (res_ac==1)
-    cnt++;
-    else
-    printf("");
-
-printf("\nNumber of pairs is... %d", cnt);
-return(0);
-}
+    printf("Square equals: %9.2f\t Volume equals: %9.2f", square, volume); 
+    return(0);
+    }
