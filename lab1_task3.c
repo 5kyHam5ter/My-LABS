7f826973d8c2b102f12e08b1d5d0aecd6e31e892
5kyHam5ter <92105017+5kyHam5ter@users.noreply.github.com>
2026-09-08 12:54:00 +0300

    Update lab1_task3.c

--- lab1_task3.c
+++ lab1_task3.c
@@ -4,35 +4,48 @@ int main() {
     float a;
     float b;
     float c;
+    int input_check_a;
+    int input_check_b;
+    int input_check_c;
     float res_ab;
     float res_bc;
     float res_ac;
     int cnt = 0;
-    
-    printf("Enter number A... ");
-    scanf("%f", &a);
-    printf("Enter number B... ");
-    scanf("%f", &b);
-    printf("Enter number C... ");
-    scanf("%f", &c);
-    
-    res_ab = a*b;
-    res_bc = b*c;
-    res_ac = a*c;
 
+    printf("Enter number A... ");
+    input_check_a = scanf("%f", &a);
+
+    if (input_check_a !=1) {
+    printf("Invalid input");
+    return 0;
+    } 
+        else
+        printf("Enter number B... ");
+        input_check_b = scanf("%f", &b);
+
+            if (input_check_b !=1) {
+                printf("Invalid input");
+                return 0;
+        }
+            else
+            printf("Enter number C... ");
+            input_check_c = scanf("%f", &c);
+                if (input_check_c !=1) {
+                    printf("\nInvalid input");
+                    return 0;
+            }          
+    res_ab = a*b;
     if (res_ab==1)
     cnt++;
-    else
-    printf("");
+    
+    res_bc = b*c;
     if (res_bc==1)
     cnt++;
-    else
-    printf("");
+    
+    res_ac = a*c;
     if (res_ac==1)
     cnt++;
-    else
-    printf("");
-    printf("Number of pairs is... %d", cnt);
-    
-    return(0);
+
+printf("\nNumber of pairs is... %d", cnt);
+return(0);
     }
