fd613f04c6c423987b65549a3d7c040c2d6d8f04
5kyHam5ter <92105017+5kyHam5ter@users.noreply.github.com>
2026-09-08 12:03:21 +0300

    Update lab1_task1.c

--- lab1_task1.c
+++ lab1_task1.c
@@ -4,15 +4,15 @@ int main() {
     float radius;
     float square;
     float volume;
+    int input_check;
     
     printf("Enter a radius... ");
-    scanf("%f", &radius);
-    
+    input_check = scanf("%f", &radius);
     square = 4*radius*radius*3.14;
-    printf("Square equals: %9.2f\n", square);
-    
     volume = 3.14*radius*radius*radius*4/3;
-    printf("Volume equals: %9.2f\n", volume);
-    
+    if (input_check != 1)
+    printf("Invalid input.");
+    else
+    printf("Square equals: %9.2f\t Volume equals: %9.2f", square, volume); 
     return(0);
     }
